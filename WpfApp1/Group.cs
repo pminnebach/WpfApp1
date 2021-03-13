72b392d217fb231a7e785df1e19d16971874c4ff
Philip Minnebach <philip@minnebach.be>
2021-03-13 12:47:35 +0100

    Add new classes for groups and users

---
+++ WpfApp1/Group.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Group : INotifyPropertyChanged
+    {
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+                this.OnPropertyChanged("FirstName");
+            }
+        }
+
+        public Group(string name)
+        {
+            _Name = name;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfApp1/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+                this.OnPropertyChanged("FirstName");
+            }
+        }
+
+        public User(string name)
+        {
+            _Name = name;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
